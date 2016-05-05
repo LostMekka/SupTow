bb096085bd2afe157c7e96851dd654d8553d20f0
Josephine Rehak <justallphine@googlemail.com>
2016-05-05 11:48:47 +0200

    created new project

---
+++ desktop/src/de/lostmekkasoft/suptow/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.lostmekkasoft.suptow.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.lostmekkasoft.suptow.SupTowGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SupTowGame(), config);
+	}
+}
